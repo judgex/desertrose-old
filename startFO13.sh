a79c8f227ec619225d20fa031fb8854b4101c289
judgex <jkjudgex@gmail.com>
2019-12-11 09:05:29 +0000

    Script update.

--- startFO13.sh
+++ startFO13.sh
@@ -3,7 +3,7 @@ if ps -ef | grep -v grep | grep DreamDaemon ; then
   echo "already running."
   exit 0
 else
-  sudo /ss13/byond/bin/DreamDaemon /ss13/fo13/desertrose/tgstation.dmb 5454 -trusted
+  DreamDaemon /ss13/fo13/desertrose/tgstation.dmb 5454 -trusted
   echo "launching"
   exit 0
 fi
