f1f21096db02e962d2c54a424f624d52529c207e
judgex <jkjudgex@gmail.com>
2019-12-11 08:41:10 +0000

    Revert "Script updates, etc for running on centos."
    This reverts commit 1a57d7292eb5a0e33769feea3dcd72b271678c15.

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,1 @@
-DreamMaker /ss13/fo13/desertrose/tgstation.dme
+sudo /ss13/byond/bin/DreamMaker /ss13/fo13/desertrose/tgstation.dme
--- startFO13.sh
+++ startFO13.sh
@@ -3,7 +3,7 @@ if ps -ef | grep -v grep | grep DreamDaemon ; then
   echo "already running."
   exit 0
 else
-  /usr/local/bin/DreamDaemon /ss13/fo13/desertrose/tgstation.dmb 5454 -trusted
+  sudo /ss13/byond/bin/DreamDaemon /ss13/fo13/desertrose/tgstation.dmb 5454 -trusted
   echo "launching"
   exit 0
 fi
